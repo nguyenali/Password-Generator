88824fa690d6833e1fa022bfc535296694aa1885
Ali Nguyen <alin57717@gmail.com>
2020-08-08 15:47:01 -0700

    make correction to loop statments

--- Develop/script.js
+++ Develop/script.js
@@ -13,128 +13,134 @@ var charLength = prompt("How many characters would you like your password to be?
 
 
 //add the end brackt later)
-if  ( charLength >= 8) && (charLength<=128) {
+if  ((charLength >= 8) && (charLength<= 128)) {
 
   //Variables for characters for password generator
-  var symbols ="`~!@#$%^&*()_-+={}[]/;:<>?/";
-  var numbers = "0123456789";
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let symbols ="`~!@#$%^&*()_-+={}[]/;:<>?/";
+  let numbers = "0123456789";
+  let lowercase = "abcdefghijklmnopqrstuvwxyz";
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  //string for new password;
+  let newPassword = "";
+
+
+  //User prompt to ask which characters they would like to use
+  let addSymbols = confirm("Would you like to add special characters? (i.e : !, #, $, *, etc..");
+  let addNumbers =confirm("Would you like to add numbers? (i.e : 1,2,3, etc..");
+  let addLowerCase = confirm("Would you like to add lowercase letters? (i.e : a, b, c, etc..");
+  let addUppercase = confirm("Would you like to add uppercase letters? (i.e : A, B, C, etc..");
   
   //Variable to add characters for password generator together
-  var a = symbols + numbers + lowercase + uppercase;
-  var b = symbols + numbers;
-  var c = symbols + uppercase;
-  var d = symbols + lowercase;
-  var e = numbers + lowercase;
-  var f = numbers + uppercase;
-  var g = lowercase + uppercase;
-  var h = symbols + numbers + lowercase;
-  var i = symbols + numbers + uppercase;
-  var j = symbols + lowercase + uppercase;
-  var k = numbers + lowercase + uppercase;
-
-  
-  //User prompt to ask which characters they would like to use
-  addSymbols = confirm("Would you like to add special characters? (i.e : !, #, $, *, etc..");
-  addNumbers =confirm("Would you like to add numbers? (i.e : 1,2,3, etc..");
-  addLowerCase = confirm("Would you like to add lowercase letters? (i.e : a, b, c, etc..");
-  addUppercase = confirm("Would you like to add uppercase letters? (i.e : A, B, C, etc..");
+  let a = symbols + numbers + lowercase + uppercase;
+  let b = symbols + numbers;
+  let c = symbols + uppercase;
+  let d = symbols + lowercase;
+  let e = numbers + lowercase;
+  let f = numbers + uppercase;
+  let g = lowercase + uppercase;
+  let h = symbols + numbers + lowercase;
+  let i = symbols + numbers + uppercase;
+  let j = symbols + lowercase + uppercase;
+  let k = numbers + lowercase + uppercase;
 
 
   //Loop statements defining password combinations
 
-  if ((addSymbols === true) && (addNumbers === true) && (addLowerCase ==== true) && (addUppercase === true)) {
+  if ((addSymbols === true) && (addNumbers === true) && (addLowerCase === true) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *a.length);
       newPassword +=a.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === true) && (addLowerCase ==== false) && (addUppercase === false)) {
+  else if ((addSymbols === true) && (addNumbers === true) && (addLowerCase === false) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *b.length);
       newPassword +=b.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase ==== false) && (addUppercase === true)) {
+  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase === false) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *c.length);
       newPassword +=c.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase ==== true) && (addUppercase === false)) {
+  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase === true) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *d.length);
       newPassword +=d.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase ==== true) && (addUppercase === false)) {
+  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase === true) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *e.length);
       newPassword +=e.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase ==== false) && (addUppercase === true)) {
+  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase === false) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *f.length);
       newPassword +=f.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false) && (addNumbers === false) && (addLowerCase ==== false) && (addUppercase === false)) {
+  else if ((addSymbols === false) && (addNumbers === false) && (addLowerCase === false) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *g.length);
       newPassword +=g.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === true) && (addLowerCase ==== true) && (addUppercase === false)) {
+  else if ((addSymbols === true) && (addNumbers === true) && (addLowerCase === true) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *h.length);
       newPassword +=h.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === true) && (addLowerCase ==== false) && (addUppercase === true)) {
+  else if ((addSymbols === true) && (addNumbers === true) && (addLowerCase === false) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *i.length);
       newPassword +=i.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase ==== true) && (addUppercase === true)) {
+  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase === true) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *j.length);
       newPassword +=j.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase ==== true) && (addUppercase === true)) {
+  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase === true) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *k.length);
       newPassword +=k.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase ==== false) && (addUppercase === false)) {
+  else if ((addSymbols === true) && (addNumbers === false) && (addLowerCase === false) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *symbols.length);
       newPassword +=symbols.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase ==== false) && (addUppercase === false)) {
+  else if ((addSymbols === false) && (addNumbers === true) && (addLowerCase === false) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *numbers.length);
       newPassword +=numbers.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false) && (addNumbers === false) && (addLowerCase ==== true) && (addUppercase === false)) {
+  else if ((addSymbols === false) && (addNumbers === false) && (addLowerCase === true) && (addUppercase === false)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *lowercase.length);
       newPassword +=lowercase.charAt(character, character+1);
     }
   }
-  else if ((addSymbols === false && (addNumbers === false) && (addLowerCase ==== false) && (addUppercase === true)) {
+  else if ((addSymbols === false) && (addNumbers === false) && (addLowerCase === false) && (addUppercase === true)) {
     for(i=0; i<charLength; i++) {
       let character = Math.floor(Math.random() *uppercase.length);
       newPassword +=uppercase.charAt(character, character+1);
     }
   }
+  
 
+
+    //Generate new password into text slot
   generatePassword.textContent = newPassword;
 
 }
@@ -144,11 +150,9 @@ if  ( charLength >= 8) && (charLength<=128) {
 
 
 
-
-
   
   
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -157,3 +161,5 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
