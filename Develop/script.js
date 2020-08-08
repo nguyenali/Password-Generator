a962a3aba4fc3249e7e0b8a0f4f6f8d2c9f1530a
Ali Nguyen <alin57717@gmail.com>
2020-08-07 23:53:50 -0700

    add variable characters for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,21 @@
-//Variable elemetns to be manipulated by Javascript
+//Variable for the elements to be manipulated by Javascript
 var generateBtn = document.querySelector("#generate");
 var generatePassword = document.querySelector("#password");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//Variables for password generator
+var Symbols ="`~!@#$%^&*()_-+={}[]/;:<>?/";
+var Numbers = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,5 +26,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
