db1b17584e2c603b021927fb6bfcc5a8843565e2
Ali Nguyen <alin57717@gmail.com>
2020-08-08 00:24:41 -0700

    add user prompt for choosing characters

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,11 @@ generateBtn.addEventListener("click", writePassword);
 
 //Prompt for desired password character length 
 
-var charlength = prompt("How many characters would you like your password to be? Password legnth can vary from 8 to 128")
+var charlength = prompt("How many characters would you like your password to be? Password length can vary from 8 to 128 (Pick a number between 8 and 128!)")
+
+
+//add the end brackt later)
+if  ( charlength >= 8 && charlength<=128 {
 
 //Variables for characters for password generator
 var symbols ="`~!@#$%^&*()_-+={}[]/;:<>?/";
@@ -29,6 +33,14 @@ var h = symbols + numbers + uppercase;
 var i = symbols + lowercase + uppercase;
 var j = numbers + lowercase + uppercase;
 var k = symbols + numbers + lowercase + uppercase;
+
+addSymbols = confirm("Would you like to add special characters? (i.e : !, #, $, *, etc..");
+
+addNumbers =confirm("Would you like to add numbers? (i.e : 1,2,3, etc..");
+
+AddLowerCase = confirm("Would you like to add lowercase letters? (i.e : a, b, c, etc..");
+
+AddUppercase - confirm("Would you like to add uppercase letters? (i.e : A, B, C, etc..");
 
 
 
